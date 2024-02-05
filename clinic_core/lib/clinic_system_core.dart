d66b4d421120cf54fb205c1356ced2191d98c25a
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-02-05 20:12:33 -0300

    export class

--- clinic_core/lib/clinic_system_core.dart
+++ clinic_core/lib/clinic_system_core.dart
@@ -1,1 +1,6 @@
 library clinic_system_core;
+
+export "./functionalPrograming/either.dart";
+export "./functionalPrograming/nil.dart";
+export "./functionalPrograming/unit.dart";
+export "./helpers/messages.dart";
