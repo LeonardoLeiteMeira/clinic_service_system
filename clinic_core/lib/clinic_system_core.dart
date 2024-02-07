a6283da459bf4654f1e1ec3d3d856785086713ca
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-02-07 15:06:57 -0300

    export

--- clinic_core/lib/clinic_system_core.dart
+++ clinic_core/lib/clinic_system_core.dart
@@ -4,3 +4,5 @@ export 'src/functionalPrograming/either.dart';
 export 'src/functionalPrograming/nil.dart';
 export 'src/functionalPrograming/unit.dart';
 export 'src/helpers/messages.dart';
+export 'src/restClient/rest_client.dart';
+export 'src/constants/local_storage_constants.dart';
