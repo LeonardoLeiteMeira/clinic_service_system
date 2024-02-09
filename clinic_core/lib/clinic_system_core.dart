8b07b2058752966bd2780cc46a1442ac0c84bf9b
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-02-09 16:02:30 -0300

    export exceptions

--- clinic_core/lib/clinic_system_core.dart
+++ clinic_core/lib/clinic_system_core.dart
@@ -7,3 +7,6 @@ export 'src/helpers/messages.dart';
 export 'src/restClient/rest_client.dart';
 export 'src/constants/local_storage_constants.dart';
 export 'src/clinic_core_config.dart';
+export 'src/exceptions/auth_exception.dart';
+export 'src/exceptions/respository_exception.dart';
+export 'src/exceptions/service_exception.dart';
