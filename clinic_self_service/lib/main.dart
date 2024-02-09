bdb74fe52f40d61505368eab06e85b267f827b26
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-02-09 15:10:40 -0300

    Add assets, set minSDK and create splash page

--- clinic_self_service/lib/main.dart
+++ clinic_self_service/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:clinic_self_service/src/core/pages/splash_page/splash_page.dart';
 import 'package:clinic_system_core/clinic_system_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
@@ -14,57 +15,8 @@ class ClinicSelfServiceApp extends StatelessWidget {
     return ClinicCoreConfig(
       title: "Auto Atendimento",
       pageBuilder: [
-        FlutterGetItPageBuilder(
-            page: (_) => const MyHomePage(title: "Auto Atendimento"), path: "/")
+        FlutterGetItPageBuilder(page: (_) => const SplashPage(), path: "/")
       ],
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
