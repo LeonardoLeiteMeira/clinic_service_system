a4036a89e6d4a45fe05664fd406d02981dabb62a
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-02-13 18:50:22 -0300

    Add zoned guarded

--- clinic_self_service/lib/main.dart
+++ clinic_self_service/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:clinic_self_service/src/binding/clinic_app_binding.dart';
 import 'package:clinic_self_service/src/modules/auth/auth_module.dart';
 import 'package:clinic_self_service/src/pages/splash_page/splash_page.dart';
@@ -6,7 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 
 void main() {
-  runApp(const ClinicSelfServiceApp());
+  runZonedGuarded(() {
+    runApp(const ClinicSelfServiceApp());
+  }, (error, stack) {
+    log('Error nao tratado', error: error, stackTrace: stack);
+    throw error;
+  });
 }
 
 class ClinicSelfServiceApp extends StatelessWidget {
