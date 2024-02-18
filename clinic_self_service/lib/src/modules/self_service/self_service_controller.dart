5e2a6585340e525792a329d3850ebea3856863ca
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-02-18 20:50:29 -0300

    create who I am page flow

--- clinic_self_service/lib/src/modules/self_service/self_service_controller.dart
+++ clinic_self_service/lib/src/modules/self_service/self_service_controller.dart
@@ -1,3 +1,4 @@
+import 'package:clinic_self_service/src/models/self_service_model.dart';
 import 'package:clinic_system_core/clinic_system_core.dart';
 import 'package:signals_flutter/signals_flutter.dart';
 
@@ -5,25 +6,17 @@ enum FormSteps { none, whoIAm, patient, findPatient, documents, done, restart }
 
 class SelfServiceController with MessageStateMixin {
   final _step = ValueSignal(FormSteps.none);
+  var _model = SelfServiceModel();
   FormSteps get step => _step.value;
 
   void startProcess() {
     _step.forceUpdate(FormSteps.whoIAm);
   }
 
-  void goPatient() {
-    _step.forceUpdate(FormSteps.patient);
-  }
+  void clearForm() => _model.clear();
 
-  void goFindPatient() {
+  void completeWhoIAmProcess(String name, String lastName) {
+    _model = _model.copyWith(name: () => name, lastName: () => lastName);
     _step.forceUpdate(FormSteps.findPatient);
-  }
-
-  void goDocuments() {
-    _step.forceUpdate(FormSteps.documents);
-  }
-
-  void goDone() {
-    _step.forceUpdate(FormSteps.done);
   }
 }
--- clinic_self_service/lib/src/modules/self_service/who_i_am/who_i_am_page.dart
+++ clinic_self_service/lib/src/modules/self_service/who_i_am/who_i_am_page.dart
@@ -1,6 +1,9 @@
 import 'package:clinic_self_service/src/modules/self_service/self_service_controller.dart';
+import 'package:clinic_system_core/clinic_system_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:validatorless/validatorless.dart';
 
 class WhoIAmPage extends StatefulWidget {
   const WhoIAmPage({super.key});
@@ -12,23 +15,114 @@ class WhoIAmPage extends StatefulWidget {
 class _WhoIAmPageState extends State<WhoIAmPage> {
   final controller = Injector.get<SelfServiceController>();
 
+  final selfServiceController = Injector.get<SelfServiceController>();
+
+  final _formKey = GlobalKey<FormState>();
+  final _nameEC = TextEditingController();
+  final _lastNameEC = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameEC.dispose();
+    _lastNameEC.dispose();
+    super.dispose();
+  }
+
+  void continueButtom() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+    if (isValid) {
+      selfServiceController.completeWhoIAmProcess(
+          _nameEC.text, _lastNameEC.text);
+    }
+  }
+
+  void onSelectPopupMenu(int value) async {
+    if (value == 1) {
+      final navigatorOf = Navigator.of(context);
+      final sp = await SharedPreferences.getInstance();
+      sp.clear();
+      navigatorOf.pushNamedAndRemoveUntil('/', (route) => false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Who I Am")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("Who I Am Page"),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: controller.goFindPatient,
-              child: const Text("Find Patient"),
-            ),
-          ],
-        ),
-      ),
+    final sizeOf = MediaQuery.of(context).size;
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) {
+        _nameEC.text = '';
+        _lastNameEC.text = '';
+        controller.clearForm();
+      },
+      child: Scaffold(
+          appBar: ClinicAppBar(actions: [
+            PopupMenuButton(
+              itemBuilder: (context) => [
+                const PopupMenuItem(value: 1, child: Text("Finalizar Terminal"))
+              ],
+              onSelected: onSelectPopupMenu,
+              child: const IconPopupCustom(),
+            )
+          ]),
+          body: LayoutBuilder(builder: (_, constraints) {
+            return SingleChildScrollView(
+              child: Container(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/background_login.png'),
+                      fit: BoxFit.cover),
+                ),
+                child: Center(
+                  child: Container(
+                    padding: const EdgeInsets.all(40),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    width: sizeOf.width * .8,
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          Image.asset('assets/images/logo_vertical.png'),
+                          const SizedBox(height: 48),
+                          const Text(
+                            'Bem Vindo!',
+                            style: ClinicTheme.titleStyle,
+                          ),
+                          const SizedBox(height: 48),
+                          TextFormField(
+                            controller: _nameEC,
+                            validator:
+                                Validatorless.required('Nome Obrigatório'),
+                            decoration: const InputDecoration(
+                                label: Text('Digite Seu Nome')),
+                          ),
+                          const SizedBox(height: 24),
+                          TextFormField(
+                            controller: _lastNameEC,
+                            validator:
+                                Validatorless.required('Sobrenome Obrigatório'),
+                            decoration: const InputDecoration(
+                                label: Text('Digite Seu Sobrenome')),
+                          ),
+                          const SizedBox(height: 24),
+                          SizedBox(
+                              width: sizeOf.width * .8,
+                              height: 48,
+                              child: ElevatedButton(
+                                  onPressed: continueButtom,
+                                  child: const Text('Continuar'))),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          })),
     );
   }
 }
