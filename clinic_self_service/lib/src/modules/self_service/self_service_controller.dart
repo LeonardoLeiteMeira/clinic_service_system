0ea9ff275e60bfb1eec9b29a8d8a19b8f3e614b3
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-02-19 16:06:42 -0300

    Update self_service_controller.dart

--- clinic_self_service/lib/src/modules/self_service/self_service_controller.dart
+++ clinic_self_service/lib/src/modules/self_service/self_service_controller.dart
@@ -1,3 +1,4 @@
+import 'package:clinic_self_service/src/models/patient_model.dart';
 import 'package:clinic_self_service/src/models/self_service_model.dart';
 import 'package:clinic_system_core/clinic_system_core.dart';
 import 'package:signals_flutter/signals_flutter.dart';
@@ -8,6 +9,7 @@ class SelfServiceController with MessageStateMixin {
   final _step = ValueSignal(FormSteps.none);
   var _model = SelfServiceModel();
   FormSteps get step => _step.value;
+  SelfServiceModel get model => _model;
 
   void startProcess() {
     _step.forceUpdate(FormSteps.whoIAm);
@@ -15,8 +17,18 @@ class SelfServiceController with MessageStateMixin {
 
   void clearForm() => _model.clear();
 
+  void restartProcess() {
+    _step.forceUpdate(FormSteps.restart);
+    clearForm();
+  }
+
   void completeWhoIAmProcess(String name, String lastName) {
     _model = _model.copyWith(name: () => name, lastName: () => lastName);
     _step.forceUpdate(FormSteps.findPatient);
+  }
+
+  void completeFindPatientProcess(PatientModel? patient) {
+    _model = _model.copyWith(patient: () => patient);
+    _step.forceUpdate(FormSteps.patient);
   }
 }
