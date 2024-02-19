d2a0fbb06c92ece4f2152309b71a2ce4ac4daadf
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-02-19 16:08:41 -0300

    Create patient_router.dart

---
+++ clinic_self_service/lib/src/modules/self_service/patient/patient_router.dart
@@ -0,0 +1,15 @@
+import 'package:clinic_self_service/src/modules/self_service/patient/patient_controller.dart';
+import 'package:clinic_self_service/src/modules/self_service/patient/patient_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+
+class PatientRouter extends FlutterGetItModulePageRouter {
+  const PatientRouter({super.key});
+
+  @override
+  List<Bind<Object>> get bindings =>
+      [Bind.lazySingleton((i) => PatientController())];
+
+  @override
+  WidgetBuilder get view => (_) => const PatientPage();
+}
