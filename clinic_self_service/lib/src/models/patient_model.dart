de839d3c29454bd22ffa2a43f25c9f85077a37a0
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2024-02-21 11:31:53 -0300

    add copyWith method to patient model

--- clinic_self_service/lib/src/models/patient_model.dart
+++ clinic_self_service/lib/src/models/patient_model.dart
@@ -31,6 +31,29 @@ class PatientModel {
   factory PatientModel.fromJson(Map<String, dynamic> json) =>
       _$PatientModelFromJson(json);
   Map<String, dynamic> toJson() => _$PatientModelToJson(this);
+
+  PatientModel copyWith({
+    String? id,
+    String? name,
+    String? email,
+    String? phoneNumber,
+    String? document,
+    PatientAddressModel? address,
+    String? guardian,
+    String? guardianIdentificationNumber,
+  }) {
+    return PatientModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+      document: document ?? this.document,
+      address: address ?? this.address,
+      guardian: guardian ?? this.guardian,
+      guardianIdentificationNumber:
+          guardianIdentificationNumber ?? this.guardianIdentificationNumber,
+    );
+  }
 }
 
 
